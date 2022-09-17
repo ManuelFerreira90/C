f428bc06f766d8c1f6dd4f205f4eb11158b96d8f
Manuel Ferreira <105729881+ManuelFerreira90@users.noreply.github.com>
2022-09-17 19:32:50 -0300

    3 exercícios em C

---
+++ Lista-Exercícios-01/01.C
@@ -0,0 +1,15 @@
+//01
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+	int a1,a2,soma;
+	scanf("%d" "%d",&a1,&a2);
+	soma = ((a1+a2)*a2)/2;
+
+	printf("Resultado = %d \n",soma);
+
+	return 0;
+}
