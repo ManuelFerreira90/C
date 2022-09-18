58ac6fc1aaebe69509a49cb13ffcc9d0bc528edc
Manuel Ferreira <105729881+ManuelFerreira90@users.noreply.github.com>
2022-09-18 00:01:31 -0300

    Indentação

--- Lista-Exercícios-01/02.C
+++ Lista-Exercícios-01/02.C
@@ -8,7 +8,7 @@ int main(){
 	printf("Digite o valor de fabrica do carro: ");
 	scanf("%f",&cinicial);
 
-    	cfinal=((cinicial*0.28)+(cinicial*0.45))+cinicial;
+    cfinal=((cinicial*0.28)+(cinicial*0.45))+cinicial;
 
 	printf("Valor final do carro: R$%2.f \n",cfinal);
 
