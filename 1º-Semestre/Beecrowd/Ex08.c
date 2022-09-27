d94524ab4f577b29e011ad1d9af8ed1f5985bcd1
Manuel Ferreira <105729881+ManuelFerreira90@users.noreply.github.com>
2022-09-27 08:59:25 -0300

    Novos Exercícios

---
+++ 1º-Semestre/Beecrowd/Ex08.c
@@ -0,0 +1,26 @@
+//1071
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int x,y,i,soma=0;
+    scanf("%d %d",&x,&y);
+
+    if(x>y){
+        for(i=y+1;i<x;i++){
+            if(i%2!=0){
+                soma+=i;
+            }
+        }
+    }
+    else{
+        for(i=x+1;i<y;i++){
+            if(i%2!=0){
+                soma+=i;
+            }
+        }
+
+    }
+    printf("%d\n",soma);
+    return 0;
+}
