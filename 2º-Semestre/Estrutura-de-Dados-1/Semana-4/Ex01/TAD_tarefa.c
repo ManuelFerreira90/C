c3ed9916f90346404af0b43f687428412345a72c
Manuel Ferreira <105729881+ManuelFerreira90@users.noreply.github.com>
2022-10-13 12:14:16 +0000

    Novos exercícios

---
+++ 2º-Semestre/Estrutura-de-Dados-1/Semana-4/Ex01/TAD_tarefa.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "TAD_tarefa.h"
+
+void iniciar(Lista_T *lista){
+  lista->nroElem = 0;
+}
+
+void inserir(Lista_T *lista, Itemtarefa *ch){
+  int j;
+  
+  if(lista->nroElem > 50){
+    prinf("Lista esta cheia.\n");
+    return
+  }
+
+  for(j=lista->nroElem; j >=0; j--){
+    lista->itarefa[j] = lista->itarefa[j-1];
+  }
+  strcpy(lista->itarefa[0], ch);
+  lista->nroElem++;
+}
+
+void remover(Lista_T *lista, Itemtarefa *ch){
+  int pos,j=0,cont =0,r,r2;
+
+  while(cont < 50){
+    r = strcmp(lista->itarefa[j], ch);
+    if(r == 0){
+      r2 = 1;
+      pos = j;
+    }
+    else j++;
+  }
+
+  if(r2 != 1){
+    printf("Palavra nao removida.\n");
+  }
+
+  for(j=pos, j<lista->nroElem; j++){
+    lista->itarefa[j] = lista->itarefa[j+1];
+  }
+  lista->nroElem--;
+  printf("Palavra removida.\n");
+}
+
+void imprimir(Lista_T *lista){
+  int i;
+  for(i=0; i<50; i++){
+    printf("Tarefa %d : %s\n", i, lista->itarefa.tarefa);
+  }
+}
+
+
+
+
+
+
+
+
+//instrucoes
+void instrucoes(){
+    printf("1 para inserir tarefa.\n"
+    "2 para remover tarefa.\n"
+    "3 para imprimir tarefas.\n"
+    "4 para finalizar.\n");
+}
---
+++ 2º-Semestre/Estrutura-de-Dados-1/Semana-4/Ex01/TAD_tarefa.h
@@ -0,0 +1,20 @@
+//declrando tipo tarefa
+typedef char Itemtarefa;
+
+//declarando estrutura tarefa
+typedef struct{
+  Itemtarefa tarefa.
+}Ttarefa;
+
+//declarando lista tarefa
+typedef struct{
+  Ttarefa itarefa[50];
+  int nroElem;
+}Lista_T;
+
+//prototipos
+void iniciar(Lista_T *lista);
+void inserir(Lista_T *lista);
+void remover(Lista_T *lista);
+void imprimir(Lista_T *lista);
+void instrucoes();
