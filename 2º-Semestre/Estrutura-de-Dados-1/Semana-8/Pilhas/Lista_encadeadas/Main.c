a09a8e2d14226a1e8c22ddf24d3543d42361a0f0
sandrin <ribeirobr009@gmail.com>
2022-10-29 19:42:26 -0300

    Novos exercícios

---
+++ 2º-Semestre/Estrutura-de-Dados-1/Semana-8/Pilhas/Lista_encadeadas/Main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <string.h>
+#include "TadPilha.h"
+
+int main(){
+    Lpilha lista;
+    Tpilha item;
+
+    iniciar(&lista);
+
+    int escolha;
+
+    instrucoes();
+    scanf("%d",&escolha);
+
+    while (escolha != 3){
+        switch (escolha){
+            case 0:
+                printf("Digite um numero.\n");
+                scanf("%d",&item.n);
+                inserir(&lista, item);
+                break;
+        
+            case 1:
+                printf("Primeiro elemento será removido.\n");
+                remover(&lista);
+                break;
+
+            case 2:
+                printf("\nFila sera mostrada.\n");
+                imprimir(&lista);
+                break;
+
+            default:
+                break;
+        }
+        printf("\n");
+        instrucoes();
+        scanf("%d",&escolha);
+    }
+
+    return 0;
+}
