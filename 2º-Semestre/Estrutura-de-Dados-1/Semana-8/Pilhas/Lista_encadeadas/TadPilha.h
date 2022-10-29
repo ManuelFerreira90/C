308b6b311dae4b1077990c85cc86668ee9c9d10a
sandrin <ribeirobr009@gmail.com>
2022-10-29 19:42:43 -0300

    Novos exercícios

---
+++ 2º-Semestre/Estrutura-de-Dados-1/Semana-8/Pilhas/Lista_encadeadas/TadPilha.h
@@ -0,0 +1,21 @@
+typedef struct{
+    int n;
+}Tpilha;
+
+typedef struct Celpilha *Apont_P;
+
+typedef struct Celpilha{
+    Tpilha num;
+    struct Celpilha *prox;
+}Cpilha;
+
+typedef struct{
+    Apont_P topo;
+}Lpilha;
+
+Apont_P vazia(Lpilha *l);
+void iniciar(Lpilha *l);
+void inserir(Lpilha *l, Tpilha obj);
+void remover(Lpilha *l);
+void imprimir(Lpilha *l);
+void instrucoes();
